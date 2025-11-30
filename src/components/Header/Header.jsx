f50d5a7521315044b433ee157419822d8d2e3c13
Philippe Gautier <philippe.gautier35@gmail.com>
2025-11-30 14:39:22 +0100

    Modification du responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logoIcon from "../../assets/Logo.png";
 import "./Header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
       {/* Barre grise en haut */}
@@ -23,22 +25,32 @@ function Header() {
           </div>
         </Link>
 
-        <nav>
+        {/* Desktop navigation */}
+        <nav className="desktop-menu">
           <ul>
-            <li>
-              <Link to="/">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/conseils">Fiches Conseil</Link>
-            </li>
-            <li>
-              <Link to="/team">Équipe</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
+            <li><Link to="/">Accueil</Link></li>
+            <li><Link to="/conseils">Fiches Conseil</Link></li>
+            <li><Link to="/team">Équipe</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </nav>
+
+        {/* Burger pour mobile */}
+        <div className="burger" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+
+        {/* Menu mobile */}
+        {menuOpen && (
+          <nav className="mobile-menu">
+            <ul>
+              <li><Link to="/" onClick={() => setMenuOpen(false)}>Accueil</Link></li>
+              <li><Link to="/conseils" onClick={() => setMenuOpen(false)}>Fiches Conseil</Link></li>
+              <li><Link to="/team" onClick={() => setMenuOpen(false)}>Équipe</Link></li>
+              <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+            </ul>
+          </nav>
+        )}
       </div>
     </header>
   );
