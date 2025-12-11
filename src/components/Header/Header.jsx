0a50d128339bda05719a46ed34850abd57ef041c
Philippe Gautier <philippe.gautier35@gmail.com>
2025-12-11 21:53:52 +0100

    Prise en compte header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -60,7 +60,8 @@ function Header() {
       </div>
 
       <div className="info-banner">
-        Site et cabinet en construction, nous serons heureux de vous accueillir à partir du <strong>2 février 2026 !</strong>  !
+        Site et cabinet en construction, nous serons heureux de vous accueillir à partir du <strong>2 février 2026 !</strong> 
+        <br>Vous pouvez d'ores et déjà prendre rendez vous</br>
       </div>
 
 
